4415a69015abf869cf5c90b748359e8b64bced2f
Patrik Spathon <patrik@spathon.com>
2017-07-22 17:40:11 +0200

    Get authors username

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,14 @@ import gql from 'graphql-tag'
 import { dateToString } from '../utils'
 
 function Post ({ post }) {
-  const { title, slug, username, created_at } = post
+  const { title, slug, author, created_at } = post
   return <div className='Post-item'>
     <h2>
       <a href={`http://localhost:3000/posts/${slug}`}>{title}</a>
     </h2>
     <div className='meta'>
       { dateToString(created_at) }
-      <a href='#Hmm' className='float-right'>{username}</a>
+      <a href='#Hmm' className='float-right'>{author.username}</a>
     </div>
   </div>
 }
@@ -23,21 +23,10 @@ function Loading () {
 
 export function Home (props) {
   const { loading, posts } = props.data
-
   if (loading) return <Loading />
 
-  // const pageCount = Math.ceil(this.state.totalPostCount / this.limit)
-  // const page = this.props.page || 1
-  //
-  // const pagination = []
-  // for (let i = 1; i <= pageCount; i++) {
-  //   pagination.push(<Link key={i} to={`/page/${i}`}> {i} </Link>)
-  // }
-
   return <div>
-    {/* <h5>Page {page} of {pageCount}</h5> */}
     {posts.map((post, i) => <Post key={i} post={post} />)}
-    {/* <p>{pagination}</p> */}
   </div>
 }
 
@@ -46,6 +35,9 @@ const postsQuery = gql`query partnerWithTariffQuery {
     title
     slug
     created_at
+    author {
+      username
+    }
   }
 }`
 
