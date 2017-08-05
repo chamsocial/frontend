ccc6b954bb3ebc1c8ae24db02d09f0f3d349e79d
Patrik Spathon <patrik@spathon.com>
2017-08-05 18:00:19 +0200

    Add some styling to loading

--- src/components/partials/Loading.js
+++ src/components/partials/Loading.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import './Loading.css'
 
 export default function Loading () {
-  return <div className='loading'>Loading...</div>
+  return <div className='loading'>
+    <div className='loading-icon' />
+    <div className='loading-text'>Loading...</div>
+  </div>
 }
