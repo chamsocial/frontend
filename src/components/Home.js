d7ba5827212d71b793634a22b4d1e0ae1563565e
Patrik Spathon <patrik@spathon.com>
2018-11-07 20:42:20 +0100

    Fix home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import Loading from './partials/Loading'
 import Pagination from './partials/Pagination'
 import PostListItem from './Posts/ListItem'
+
 const { REACT_APP_ITEMS_PER_PAGE } = process.env
 
-export function Home (props) {
-  const { page = 1 } = props
-  const { error, loading, posts, postsInfo } = props.data
+export function Home(props) {
+  const { page, data } = props
+  const {
+    error, loading, posts, postsInfo,
+  } = data
   if (loading || error) return <Loading error={error} />
 
-  return <div>
-    {posts.map((post, i) => <PostListItem key={i} post={post} />)}
-    <div>
-      <Pagination totalCount={postsInfo.count} page={page} itemsPerPage={REACT_APP_ITEMS_PER_PAGE} urlPrefix='/page/' />
-    </div>
-  </div>
+  return (
+    <>
+      {posts.map(post => <PostListItem key={post.id} post={post} />)}
+      <div>
+        <Pagination totalCount={postsInfo.count} page={page} itemsPerPage={REACT_APP_ITEMS_PER_PAGE} urlPrefix="/page/" />
+      </div>
+    </>
+  )
+}
+Home.defaultProps = {
+  page: 1,
+}
+Home.propTypes = {
+  page: PropTypes.number,
+  data: PropTypes.shape({
+    error: PropTypes.any,
+    loading: PropTypes.bool,
+  }).isRequired,
 }
 
-const postsQuery = gql`query postsQuery($offset: Int!) {
-  posts(limit: ${REACT_APP_ITEMS_PER_PAGE}, order: "reverse:created_at", offset: $offset) {
+const postsQuery = gql`query postsQuery($page: Int!) {
+  posts(limit: ${REACT_APP_ITEMS_PER_PAGE}, page: $page) {
     id
     title
     slug
@@ -38,13 +54,8 @@ const postsQuery = gql`query postsQuery($offset: Int!) {
 }`
 
 export default graphql(postsQuery, {
-  options: (data) => {
+  options: data => {
     const { page = 1 } = data
-    const offset = REACT_APP_ITEMS_PER_PAGE * (page - 1)
-    return {
-      variables: {
-        offset
-      }
-    }
-  }
+    return { variables: { page: parseInt(page, 10) } }
+  },
 })(Home)
