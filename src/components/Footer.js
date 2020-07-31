83fd0ce0728d1ba3586ee6d80076e9263f56a944
Patrik Spathon <patrik@spathon.com>
2020-07-31 11:46:41 +0200

    Add link to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from './github-icon.svg'
 
 
 const Footer = () => (
   <div className="container">
     <div className="footer">
       <span>
-        &copy; ChamSocial 2019
+        &copy; ChamSocial {(new Date()).getFullYear()}
+      </span>
+      <span>
+        <a href="https://github.com/chamsocial" target="_blank" rel="noreferrer noopener">
+          <img src={Logo} alt="Github.com" style={{ width: '16px' }} />
+        </a>
       </span>
       <span>
         <Link to="/about">About</Link>
