0a0042752e81be4aa19cbaa69ae16dc05e1fb5c4
Patrik Spathon <patrik@spathon.com>
2020-07-31 11:49:13 +0200

    Update about page

--- src/pages/General/About.js
+++ src/pages/General/About.js
@@ -6,7 +6,7 @@ function About() {
       <h1>About</h1>
       <p>
         Chamonix is one of the most notorious ski resorts in the world and
-        Chamsocial is Chamonix online. You can get in touch with over 11 500 users that
+        Chamsocial is Chamonix online. You can get in touch with over 16 000 users that
         help each other with accommodation, bank issues or finding each others lost dogs.
         <br />
         Get started and join us now.
@@ -19,29 +19,39 @@ function About() {
         During the summer 2014, the new directions of Chamsocial was set by three passionate
         inhabitants of Chamonix and the decision to make a new platform was made.
       </p>
-      <table className="table">
+      <table className="table table--striped">
         <tbody>
-          <tr className="small">
-            <td>ChamShare was created.</td>
-            <td>Chamsocial took over.</td>
-            <td>Chamsocial get new admins.</td>
-            <td>New version of Chamsocial was launched.</td>
-            <td>Development of this version of ChamSocial.</td>
-          </tr>
           <tr>
             <td>1999</td>
             <td>2011</td>
             <td>2014</td>
             <td>2015</td>
             <td>2019</td>
+            <td>2020</td>
+          </tr>
+          <tr className="small">
+            <td>ChamShare was created.</td>
+            <td>Chamsocial took over.</td>
+            <td>Chamsocial get new admins.</td>
+            <td>New version of Chamsocial was launched.</td>
+            <td>Development of this version of ChamSocial.</td>
+            <td>July 24 current version was deployed</td>
           </tr>
         </tbody>
       </table>
 
+      <p>
+        All code are available on {' '}
+        <a href="https://github.com/chamsocial" target="_blank" rel="noreferrer noopener">
+          Github
+        </a>.
+        {' '}Pull requests are welcome!
+      </p>
+
       <h2>Contact</h2>
       <p>
         If you have any questions or want to get in contact you can email the address:
-        <a href="mailto:Chamsocial@theskilocker.com">Chamsocial@theskilocker.com</a>
+        {' '}<a href="mailto:Chamsocial@theskilocker.com">chamsocial@spathon.com</a>
       </p>
     </div>
   )
