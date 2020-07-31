3e2a3f4b8c020937b44f239f9b632d44af248875
Patrik Spathon <patrik@spathon.com>
2020-07-31 10:21:45 +0200

    Update 404

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-  faTimes, faTimesCircle, faTrashAlt, faComment, faBell, faImage,
+  faTimes, faTimesCircle, faTrashAlt, faComment, faBell, faImage, faMapSigns,
 } from '@fortawesome/free-solid-svg-icons'
 import apollo from './utils/apollo'
 import Auth from './components/Auth'
 import Router from './Router'
 
-library.add(faTimes, faTimesCircle, faTrashAlt, faComment, faBell, faImage)
+library.add(faTimes, faTimesCircle, faTrashAlt, faComment, faBell, faImage, faMapSigns)
 
 // Prevent file drag'n drop to load file in the browser
 window.addEventListener('dragover', e => e.preventDefault(), false)
--- src/pages/Auth/FourOhFour.js
+++ src/pages/Auth/FourOhFour.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
 
 function FourOhFour() {
   return (
     <div className="text-center box">
+      <div style={{ fontSize: '70px', marginBottom: '20px', color: '#999' }}>
+        <FontAwesomeIcon icon="map-signs" />
+      </div>
       <h1>404 - Not found</h1>
       <p>
-        This site is in beta and are bing developed so the page might be missing or not created yet.
+        You must have taken a wrong turn or hit an abandoned path.
+      </p>
+      <p>
+        <Link to="/">Home</Link> | <Link to="/posts">Posts</Link> | <Link to="/signup">Signup</Link>
       </p>
     </div>
   )
