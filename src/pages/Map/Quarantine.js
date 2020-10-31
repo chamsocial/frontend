66e49ef01c26ce4c00066cf13f968b76abb6163e
Patrik Spathon <patrik@spathon.com>
2020-10-31 15:08:20 +0100

    Add search and elevation

--- src/pages/Map/Quarantine.js
+++ src/pages/Map/Quarantine.js
@@ -1,10 +1,31 @@
 /* eslint-disable no-console */
 /* globals google */
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 
 const { REACT_APP_GOOGLE_MAPS_API_KEY } = process.env
 const chamonixCenter = { lat: 45.923183187683215, lng: 6.869751360383987 }
+
+
+// eslint-disable-next-line one-var, one-var-declaration-per-line
+let map, marker, bufferCircle, infowindow
+
+
+function setMarker(latLng, includeMarker = true) {
+  if (includeMarker) marker.setPosition(latLng)
+  bufferCircle.setCenter(latLng)
+  map.panTo(latLng)
+
+  const elevator = new google.maps.ElevationService()
+  elevator.getElevationForLocations({ locations: [latLng] }, (results, status) => {
+    if (status === 'OK' && results[0] && results[0].elevation) {
+      infowindow.setContent(`<strong>Elevation:</strong> ${Math.ceil(results[0].elevation)}m`)
+      infowindow.open(map, marker)
+    } else {
+      console.log('Elevation service failed due to:', status)
+    }
+  })
+}
 
 
 function getLocation() {
@@ -23,11 +44,11 @@ function getLocation() {
 }
 
 
-function addMarker(map, geo) {
+function addMarker(geo) {
   const centre = new google.maps.LatLng(geo.lat, geo.lng)
 
   // Drop a pin at the user location
-  const marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position: centre,
     map,
     animation: google.maps.Animation.DROP,
@@ -36,7 +57,7 @@ function addMarker(map, geo) {
   })
 
   // Create a 1km circle around the user
-  const bufferCircle = new google.maps.Circle({
+  bufferCircle = new google.maps.Circle({
     center: centre,
     radius: 1000, // meters
     strokeColor: '#61a0e9',
@@ -48,25 +69,24 @@ function addMarker(map, geo) {
     visible: false,
   })
 
+  infowindow = new google.maps.InfoWindow({
+    content: '<strong>Your location</strong><br>You can drag me!',
+  })
   setTimeout(() => {
-    const infowindow = new google.maps.InfoWindow({
-      content: '<strong>Your location</strong><br>You can drag me!',
-    })
     infowindow.open(map, marker)
-    setTimeout(() => infowindow.close(), 5000)
     bufferCircle.setVisible(true)
   }, 600)
 
   // Move the circle after moving the pin
   google.maps.event.addListener(marker, 'dragend', event => {
     console.log('New pos', event.latLng.lat(), event.latLng.lng())
-    bufferCircle.setCenter(event.latLng)
+    setMarker(event.latLng, true)
   })
 }
 
 
 async function main(mapContainer) {
-  const map = new google.maps.Map(mapContainer, {
+  map = new google.maps.Map(mapContainer, {
     zoom: 14,
     center: chamonixCenter,
     styles: [{
@@ -81,12 +101,19 @@ async function main(mapContainer) {
     : chamonixCenter
   console.log('Pos', pos, 'Geo', geo)
 
-  addMarker(map, geo)
+  addMarker(geo)
 }
 
 
 function Quarantine() {
+  const [address, setAddress] = useState('')
   const mapContainer = useRef()
+  const geocoderRef = useRef()
+
+  const lockdown = new Date(1604016000 * 1000)
+  const difference = Date.now() - lockdown.getTime()
+  const dayOfLockdown = Math.ceil(difference / (1000 * 3600 * 24))
+
 
   useEffect(() => {
     if (window.initQuarantineMap || !mapContainer.current) return
@@ -94,20 +121,54 @@ function Quarantine() {
     script.src = `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initQuarantineMap`
     script.defer = true
     window.initQuarantineMap = () => {
+      geocoderRef.current = new google.maps.Geocoder()
       main(mapContainer.current)
         .catch(err => console.log('Failed:', err))
     }
     document.head.appendChild(script)
   }, [mapContainer])
 
+  function onSubmit(evt) {
+    evt.preventDefault()
+    if (!geocoderRef.current) return
+    geocoderRef.current.geocode({ address, region: 'FR' }, (results, status) => {
+      if (status !== 'OK' || !results[0]) return
+      setMarker(results[0].geometry.location)
+    })
+  }
+
   return (
     <div>
       <div className="space-between">
-        <h1>Map</h1>
-        {/* <div>
-          <input className="input" placeholder="Address" />
-        </div> */}
+        <h1>Lockdown map</h1>
+        <h1>
+          Day <strong>{dayOfLockdown}</strong> of lockdown
+        </h1>
       </div>
+
+      <div className="box box--padded box--row space-between lockdown">
+        <div className="lockdown__text">
+          <p>
+            This is an interactive map where you can drag
+            {' '}the pin and the circle has a 1km radius.
+          </p>
+          <small>Please use sparsely to prevent hitting our daily google maps API limt.</small>
+        </div>
+        <form className="lockdown__form" onSubmit={onSubmit}>
+          <label htmlFor="address">Address</label>
+          <div className="lockdown__group">
+            <input
+              placeholder="Enter a french address"
+              className="input"
+              id="address"
+              value={address}
+              onChange={evt => setAddress(evt.target.value)}
+            />
+            <button type="submit" className="btn">Move marker</button>
+          </div>
+        </form>
+      </div>
+
       <div className="quarantine-map" ref={mapContainer} />
     </div>
   )
