89ef83796737be83c7d4e6365bf2f54271365005
Patrik Spathon <patrik@spathon.com>
2020-12-18 17:48:28 +0100

    WIP countdown

--- src/pages/Map/AllowedOutside.js
+++ src/pages/Map/AllowedOutside.js
@@ -1,19 +1,56 @@
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
 import { Link } from 'react-router-dom'
 import './allowed.scss'
 
 
-function AllowedOutside() {
+function setTime(date, hours) {
+  date.setHours(date.getHours() + hours)
+}
+function padZero(time) {
+  return String(time).padStart(2, '0')
+}
+
+
+function getCurfew() {
   const date = new Date()
   const hour = date.getUTCHours() + 1
-  const isCurfew = hour >= 20 || hour < 6
+  const is = hour >= 20 || hour < 6
+  const today = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+
+  if (is && hour < 6) setTime(today, 6)
+  else if (is && hour >= 20) setTime(today, 10)
+  else setTime(today, 20)
+
+  const timeLeft = today.getTime() - date.getTime()
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
+  const countdown = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`
+
+  return { is, countdown }
+}
+
+
+function AllowedOutside() {
+  const [isCurfew, setCurfew] = useState(getCurfew())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurfew(getCurfew())
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div className={`allowed-outside ${isCurfew ? 'no' : 'yes'}`}>
+    <div className={`allowed-outside ${isCurfew.is ? 'no' : 'yes'}`}>
       <Link to="/" className="allowed-outside__close">
         <FontAwesomeIcon icon="times" />
       </Link>
-      {isCurfew ? 'No' : 'Yes'}
+      {isCurfew.is ? 'No' : 'Yes'}
+      <div className="countdown">
+        {isCurfew.countdown}
+      </div>
     </div>
   )
 }
