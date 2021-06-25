750a56e42c182d3d99bb2287cd87515021d2b64b
Patrik Spathon <patrik@spathon.com>
2021-06-25 14:00:37 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-export BUILD_PATH="./build-$(date +"%d-%m-%Y")"
+export BUILD_PATH="./build-$(date +"%d-%m-%Y-t-%H-%M")"
 
 echo "- Build"
 npm run build
 
 echo " - Symlink"
-ln -fs $BUILD_PATH build
-# ln -fs $BUILD_PATH newBuild
-# mv -T newBuild build
+ln -s $BUILD_PATH build-new
+mv -T build-new build
+
+echo " - Finished!"
