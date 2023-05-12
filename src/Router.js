fa2142c56cb3a6a1e44e056ae61e2b482897a273
spathon <patrik@spathon.com>
2023-05-12 16:59:56 +0200

    Add beta text

--- src/Router.js
+++ src/Router.js
@@ -10,6 +10,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Header />
+      <h3 style={{ textAlign: 'center', color: 'red' }}>Beta test site data will be erased!</h3>
       <div className="container">
         <FlashMessages />
         <Routes />
